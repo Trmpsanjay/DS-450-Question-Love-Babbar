f938471cf960b7fa4ed6b10123c5fda0ca3a6a1a
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2021-01-01 20:25:51 +0530

    Create countSqiares.java

---
+++ Searching And Sorting/countSqiares.java
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.countSquares(N));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int countSquares(int N) {
+        int count = 0;
+        for(int i = 1; i<N; i++){
+            if(i*i<N)
+                count++;
+            else
+                break;
+        }
+          return count;
+    }
+  
+}
