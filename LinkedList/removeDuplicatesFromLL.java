4dbfe79674d313ed6ac506a7dfb07a916b3e8649
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2021-02-07 22:21:36 +0530

    Create removeDuplicatesFromLL.java

---
+++ LinkedList/removeDuplicatesFromLL.java
@@ -0,0 +1,93 @@
+// { Driver Code Starts
+/* package whatever; // don't place package name! */
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class Remove_Duplicate_From_LL
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			Remove_Duplicate_From_LL llist = new Remove_Duplicate_From_LL(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        GfG g = new GfG();
+		llist.head = g.removeDuplicates(llist.head);
+		llist.printList();
+		
+        t--;		
+        }
+    }}// } Driver Code Ends
+
+
+/*
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+*/
+
+class GfG
+{
+    // head: head node
+    Node removeDuplicates(Node root)
+    {
+	// Your code here	
+	    if(root==null || root.next == null)
+	        return root;
+        Node temp = root;
+        while(temp != null){
+            
+            if(temp.next !=null && temp.data == temp.next.data){
+                temp.next = temp.next.next;
+            }else{
+                temp = temp.next;
+            }
+            
+        }
+        return root;
+    }
+}
