2b3ec386ffe734721aba2353134573f759f6b48e
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2021-02-07 23:29:43 +0530

    Create intersectionOfTwoLL.java

---
+++ LinkedList/intersectionOfTwoLL.java
@@ -0,0 +1,108 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node 
+{
+    int data;
+    Node next;
+    
+    Node(int a)
+        {
+            data = a;
+            next = null;
+        }
+}
+class GfG
+{
+    static Scanner sc = new Scanner(System.in);
+    public static Node inputList(int size)
+    {
+        Node head, tail;
+        int val;
+        
+        val = sc.nextInt();
+        
+        head = tail = new Node(val);
+        
+        size--;
+        
+        while(size-->0)
+        {
+            val = sc.nextInt();
+            tail.next = new Node(val);
+            tail = tail.next;
+        }
+        
+        return head;
+    }
+    
+    public static void printList(Node n)
+    {
+        while(n!=null)
+        {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+    
+    public static void main(String args[])
+        {
+            
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n , m;
+                    
+                    n = sc.nextInt();
+                    m = sc.nextInt();
+                    
+                    Node head1 = inputList(n);
+                    Node head2 = inputList(m);
+                    
+                    Sol obj = new Sol();
+                    
+                    Node result = obj.findIntersection(head1, head2);
+	    
+	                printList(result);
+	                System.out.println();
+                }
+        }
+}// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Sol
+{
+   public static Node findIntersection(Node head1, Node head2)
+    {
+        // code here.
+        Node head = null;
+        Node tail = null;
+        
+        while(head1 != null && head2 != null){
+            if(head1.data<head2.data){
+                head1 = head1.next;
+            }else if(head1.data>head2.data){
+                head2 = head2.next;
+            }else{
+                if(head==null){
+                    head=tail=head1;
+                }else{
+                    tail.next = head1;
+                    tail = tail.next;
+                }
+                head1 = head1.next;
+                head2 = head2.next;
+            }
+        }
+        
+        return head;
+    }
+}
