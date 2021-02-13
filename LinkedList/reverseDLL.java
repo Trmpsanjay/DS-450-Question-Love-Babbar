7dc5c659776db4f158ec4cf8e77abcf60fa9a758
Sanjay Kumar <trmpsanjay@gmail.com>
2021-02-13 16:52:24 +0530

    reverse DLL added

---
+++ LinkedList/reverseDLL.java
@@ -0,0 +1,133 @@
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next, prev;
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            String str[] = read.readLine().trim().split(" ");
+            Node head = null, tail = null;
+            for(int i = 0; i < n; i++)
+            {
+                int data = Integer.parseInt(str[i]);
+                if (head == null) 
+                {
+                    head = new Node(data);
+                    tail = head;
+                }
+                else
+                {
+                    tail.next = new Node(data);
+                    tail.next.prev=tail;
+                    tail = tail.next;
+                }
+            }
+            head=reverseDLL(head);
+            if(verify(head))
+    	    displayList(head);
+    	    else
+    	    System.out.print("Your pointers are not correctly connected");
+            System.out.println();
+                
+        }
+    }
+    
+    
+    
+public static void displayList(Node head)
+{
+    //Head to Tail
+    while(head.next != null)
+    {
+        System.out.print(head.data+" ");
+        head = head.next;
+    }
+    System.out.print(head.data);
+   
+}
+
+
+ // } Driver Code Ends
+
+
+/*
+class Node
+{
+    int data;
+    Node next, prev;
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+*/
+public static Node reverseDLL(Node  head)
+{
+    //base case
+    if(head==null)
+        return null;
+    
+    Node temp = head.next;
+    head.next = head.prev;  
+    head.prev = temp;
+    if (head.prev == null)  
+        return head; 
+    
+   return reverseDLL(head.prev);
+    
+}
+
+
+// { Driver Code Starts.
+
+public static boolean verify(Node head)
+{
+    int fl=0;
+    int bl=0;
+    
+    Node temp=head;
+    
+    while(temp.next!=null)
+    {
+        temp=temp.next;
+        fl++;
+    }
+    
+    while(temp.prev!=null)
+    {
+        temp=temp.prev;
+        bl++;
+    }
+    
+    return fl==bl;
+}
+}
+
+  // } Driver Code Ends
